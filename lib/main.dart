440f9fb640289a1b1637453e220ffa3630b077e3
PriescaLeylya <priescaleylya@gmail.com>
2022-05-24 14:40:00 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
-import 'package:flutter/material.dart'; 
-import 'pages/login_page.dart'; 
-void main() => runApp(MyApp()); 
-class MyApp extends StatelessWidget { 
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase/pages/login_page.dart';
+
+//void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) { 
-    return MaterialApp( 
-      debugShowCheckedModeBanner: false, 
-      title: 'Flutter Login', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-      ), 
-      home: LoginPage(), 
-    ); 
-  } 
-} 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,73 +1,129 @@
-import 'package:flutter/material.dart'; 
-import 'package:flutter_firebase/helper/sign_in.dart'; 
-import 'package:flutter_firebase/first_screen.dart'; 
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/helper/sign_in.dart';
+import 'package:flutter_firebase/pages/first_screen.dart';
 
-class LoginPage extends StatefulWidget { 
+class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState(); 
-} 
+  _LoginPageState createState() => _LoginPageState();
+}
 
-class _LoginPageState extends State<LoginPage> { 
+class _LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      body: Container( 
-        color: Colors.white, 
-        child: Center( 
-          child: Column( 
-          mainAxisSize: MainAxisSize.max, 
-          mainAxisAlignment: MainAxisAlignment.center, 
-          children: <Widget>[ 
-            FlutterLogo(size: 150), 
-            SizedBox(height: 50), 
-            _signInButton(), 
-          ], 
-          ), 
-        ), 
-      ), 
-    ); 
-  } 
-  Widget _signInButton() { 
-    return OutlineButton( 
-      splashColor: Colors.grey, 
-      onPressed: () { 
-        signInWithGoogle().then((result) { 
-          if (result != null) { 
-            Navigator.of(context).push( 
-              MaterialPageRoute( 
-                builder: (context) { 
-                  return FirstScreen(); 
-                }, 
-              ), 
-            ); 
-          } 
-        }); 
-      }, 
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)), 
-      highlightElevation: 0, 
-      borderSide: BorderSide(color: Colors.grey), 
-        child: Padding( 
-          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10), 
-          child: Row( 
-            mainAxisSize: MainAxisSize.min, 
-            mainAxisAlignment: MainAxisAlignment.center, 
-              children: <Widget>[ 
-                Image(image: AssetImage("build/flutter_assets/images/google_logo.png"), height: 35.0), 
-                Padding( 
-                  padding: const EdgeInsets.only(left: 10), 
-                  child: Text( 
-                    'Sign in with Google', 
-                    style: TextStyle( 
-                      fontSize: 20, 
-                      color: Colors.grey, 
-                    ), 
-                  ), 
-                ) 
-              ], 
-          ), 
-        ), 
-      ); 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                child: TextFormField(
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                    hintText: "masukan email",
+                    labelText: "Email",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                child: TextFormField(
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                    hintText: "masukan password",
+                    labelText: "Password",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 15),
+                  width: 250,
+                  height: 55,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 69, 165, 233),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                ),
+                 Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 15),
+                child: const Text(
+                  "or",
+                  style: TextStyle(fontSize: 20),
+                )
+                ),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  signInWithGoogle() {} 
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return FirstScreen();
+                },
+              ),
+            );
+          }
+        });
+      },
+      
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
